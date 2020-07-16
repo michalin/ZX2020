e3a4608ad18b20e079da2e09a0e175fc1c37fe06
Doctor Volt <62021547+michalin@users.noreply.github.com>
2020-07-16 18:58:35 +0200

    Add files via upload

---
+++ serdisk.h
@@ -0,0 +1,26 @@
+/* Copyright (C) 2020  Doctor Volt
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef SERDISK_H
+#define SERDISK_H
+
+#include <Arduino.h>
+#include "Z80.h"
+
+void mount();
+void unmount();
+
+#endif
